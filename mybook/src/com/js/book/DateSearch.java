096581768f2d6add495c1d9de17694ac3b1ebc1f
apple <apple@192.168.31.219>
2017-08-07 12:04:03 +0200

    Bookstore_Mgmt_System_en

---
+++ mybook/src/com/js/book/DateSearch.java
@@ -0,0 +1,19 @@
+package com.js.book;
+
+public class DateSearch {
+	private String date;
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "DateSearch [date=" + date + "]";
+	}
+
+}
